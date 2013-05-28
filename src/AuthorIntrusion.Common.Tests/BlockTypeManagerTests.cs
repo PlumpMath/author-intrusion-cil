c7b879bb6a816a865dbe396f0150fd68a67792cf
Dylan R. E. Moonfire <d.moonfire@mfgames.com>
2013-05-28 18:01:03 -0500

    Added in a stub for unit testing.

---
+++ src/AuthorIntrusion.Common.Tests/BlockTypeManagerTests.cs
@@ -0,0 +1,27 @@
+﻿// Copyright 2012-2013 Moonfire Games
+// Released under the MIT license
+// http://mfgames.com/author-intrusion/license
+
+using NUnit.Framework;
+
+namespace AuthorIntrusion.Common.Tests
+{
+	[TestFixture]
+	public class BlockTypeManagerTests
+	{
+		[Test]
+		public void CreateEmptyBlockTypeManager()
+		{
+			// Arrange
+			var project = new Project();
+
+			// Act
+			var manager = new BlockTypeManager(project);
+
+			// Assert
+			Assert.AreEqual(
+				project,
+				manager.Project);
+		}
+	}
+}
