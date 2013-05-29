4df141ad6f1f1a074906716a2e365a2c31344fcf
Dylan R. E. Moonfire <d.moonfire@mfgames.com>
2013-05-29 14:35:42 -0500

    Added in a block type change operation.
    - Included unit tests for the operation.

---
+++ src/AuthorIntrusion.Common.Tests/ChangeBlockTypeCommandTests.cs
@@ -0,0 +1,163 @@
+﻿// Copyright 2012-2013 Moonfire Games
+// Released under the MIT license
+// http://mfgames.com/author-intrusion/license
+
+using AuthorIntrusion.Common.Blocks;
+using AuthorIntrusion.Common.Commands;
+using NUnit.Framework;
+
+namespace AuthorIntrusion.Common.Tests
+{
+	[TestFixture]
+	public class ChangeBlockTypeCommandTests: CommonMultilineTests
+	{
+		#region Methods
+
+		[Test]
+		public void TestCommand()
+		{
+			// Arrange
+			BlockOwnerCollection blocks;
+			BlockCommandSupervisor commands;
+			BlockTypeSupervisor blockTypes;
+			SetupMultilineTest(out blocks, out blockTypes, out commands);
+
+			// Act
+			var command = new ChangeBlockTypeCommand(
+				blocks[0].BlockKey, blockTypes.Paragraph);
+			commands.Do(command);
+
+			// Assert
+			Assert.AreEqual(4, blocks.Count);
+
+			int index = 0;
+			Assert.AreEqual("Line 1", blocks[index].Text);
+			Assert.AreEqual(blockTypes.Paragraph, blocks[index].BlockType);
+
+			index++;
+			Assert.AreEqual("Line 2", blocks[index].Text);
+			Assert.AreEqual(blockTypes.Scene, blocks[index].BlockType);
+
+			index++;
+			Assert.AreEqual("Line 3", blocks[index].Text);
+			Assert.AreEqual(blockTypes.Scene, blocks[index].BlockType);
+
+			index++;
+			Assert.AreEqual("Line 4", blocks[index].Text);
+			Assert.AreEqual(blockTypes.Scene, blocks[index].BlockType);
+		}
+
+		[Test]
+		public void TestUndoCommand()
+		{
+			// Arrange
+			BlockOwnerCollection blocks;
+			BlockCommandSupervisor commands;
+			BlockTypeSupervisor blockTypes;
+			SetupMultilineTest(out blocks, out blockTypes, out commands);
+
+			var command = new ChangeBlockTypeCommand(
+				blocks[0].BlockKey, blockTypes.Paragraph);
+			commands.Do(command);
+
+			// Act
+			commands.Undo();
+
+			// Assert
+			Assert.AreEqual(4, blocks.Count);
+
+			int index = 0;
+			Assert.AreEqual("Line 1", blocks[index].Text);
+			Assert.AreEqual(blockTypes.Chapter, blocks[index].BlockType);
+
+			index++;
+			Assert.AreEqual("Line 2", blocks[index].Text);
+			Assert.AreEqual(blockTypes.Scene, blocks[index].BlockType);
+
+			index++;
+			Assert.AreEqual("Line 3", blocks[index].Text);
+			Assert.AreEqual(blockTypes.Scene, blocks[index].BlockType);
+
+			index++;
+			Assert.AreEqual("Line 4", blocks[index].Text);
+			Assert.AreEqual(blockTypes.Scene, blocks[index].BlockType);
+		}
+
+		[Test]
+		public void TestUndoRedoCommand()
+		{
+			// Arrange
+			BlockOwnerCollection blocks;
+			BlockCommandSupervisor commands;
+			BlockTypeSupervisor blockTypes;
+			SetupMultilineTest(out blocks, out blockTypes, out commands);
+
+			var command = new ChangeBlockTypeCommand(
+				blocks[0].BlockKey, blockTypes.Paragraph);
+			commands.Do(command);
+			commands.Undo();
+
+			// Act
+			commands.Redo();
+
+			// Assert
+			Assert.AreEqual(4, blocks.Count);
+
+			int index = 0;
+			Assert.AreEqual("Line 1", blocks[index].Text);
+			Assert.AreEqual(blockTypes.Paragraph, blocks[index].BlockType);
+
+			index++;
+			Assert.AreEqual("Line 2", blocks[index].Text);
+			Assert.AreEqual(blockTypes.Scene, blocks[index].BlockType);
+
+			index++;
+			Assert.AreEqual("Line 3", blocks[index].Text);
+			Assert.AreEqual(blockTypes.Scene, blocks[index].BlockType);
+
+			index++;
+			Assert.AreEqual("Line 4", blocks[index].Text);
+			Assert.AreEqual(blockTypes.Scene, blocks[index].BlockType);
+		}
+
+		[Test]
+		public void TestUndoRedoUndoCommand()
+		{
+			// Arrange
+			BlockOwnerCollection blocks;
+			BlockCommandSupervisor commands;
+			BlockTypeSupervisor blockTypes;
+			SetupMultilineTest(out blocks, out blockTypes, out commands);
+
+			var command = new ChangeBlockTypeCommand(
+				blocks[0].BlockKey, blockTypes.Paragraph);
+			commands.Do(command);
+			commands.Undo();
+			commands.Redo();
+
+			// Act
+			commands.Undo();
+
+			// Assert
+			Assert.AreEqual(4, blocks.Count);
+
+			int index = 0;
+			Assert.AreEqual("Line 1", blocks[index].Text);
+			Assert.AreEqual(blockTypes.Chapter, blocks[index].BlockType);
+
+			index++;
+			Assert.AreEqual("Line 2", blocks[index].Text);
+			Assert.AreEqual(blockTypes.Scene, blocks[index].BlockType);
+
+			index++;
+			Assert.AreEqual("Line 3", blocks[index].Text);
+			Assert.AreEqual(blockTypes.Scene, blocks[index].BlockType);
+
+			index++;
+			Assert.AreEqual("Line 4", blocks[index].Text);
+			Assert.AreEqual(blockTypes.Scene, blocks[index].BlockType);
+		}
+
+		#endregion
+	}
+}
---
+++ src/AuthorIntrusion.Common/Commands/ChangeBlockTypeCommand.cs
@@ -0,0 +1,56 @@
+﻿// Copyright 2012-2013 Moonfire Games
+// Released under the MIT license
+// http://mfgames.com/author-intrusion/license
+
+using AuthorIntrusion.Common.Blocks;
+
+namespace AuthorIntrusion.Common.Commands
+{
+	/// <summary>
+	/// A command to change the type of block to a given type.
+	/// </summary>
+	public class ChangeBlockTypeCommand: SingleBlockCommand
+	{
+		#region Properties
+
+		public BlockType BlockType { get; private set; }
+
+		public override bool IsUndoable
+		{
+			get { return true; }
+		}
+
+		#endregion
+
+		#region Methods
+
+		protected override void Do(
+			Project project,
+			Block block)
+		{
+			block.BlockType = BlockType;
+		}
+
+		protected override IBlockCommand GetInverseCommand(
+			Project project,
+			Block block)
+		{
+			var inverse = new ChangeBlockTypeCommand(block.BlockKey, block.BlockType);
+			return inverse;
+		}
+
+		#endregion
+
+		#region Constructors
+
+		public ChangeBlockTypeCommand(
+			BlockKey key,
+			BlockType blockType)
+			: base(key)
+		{
+			BlockType = blockType;
+		}
+
+		#endregion
+	}
+}
