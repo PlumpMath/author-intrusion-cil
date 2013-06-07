9c5afda884cb3e178dc68d56b1bb3aa120f294d3
Dylan R. E. Moonfire <d.moonfire@mfgames.com>
2013-06-06 23:37:59 -0500

    Adding in some basic project macros and unit tests.

---
+++ src/AuthorIntrusion.Common/Persistence/FilesystemPersistenceSettings.cs
@@ -0,0 +1,46 @@
+﻿// Copyright 2012-2013 Moonfire Games
+// Released under the MIT license
+// http://mfgames.com/author-intrusion/license
+
+namespace AuthorIntrusion.Common.Persistence
+{
+	/// <summary>
+	/// Defines the serializable settings used to control the filesystem persistence
+	/// project plugin.
+	/// 
+	/// All of the string variables in here can have project macros in them, which are
+	/// expanded at the point of saving.
+	/// </summary>
+	public class FilesystemPersistenceSettings
+	{
+		#region Properties
+
+		/// <summary>
+		/// Gets or sets the external blocks directory. This is the directory where
+		/// external blocks (e.g., chapters that are extracted from the main file) are
+		/// stored.
+		/// 
+		/// If this is null or empty, then external files are not allowed. This will
+		/// have macros expanded.
+		/// </summary>
+		public string ExternalBlocksDirectory { get; set; }
+
+		/// <summary>
+		/// Gets or sets the external settings directory. This is the directory
+		/// where non-project settings are stored. Macros inside the variable will
+		/// be expanded at the point of saving. If this is blank or null, then the
+		/// project settings will be stored in the project file.
+		/// </summary>
+		public string ExternalSettingsDirectory { get; set; }
+
+		/// <summary>
+		/// Gets or sets the project settings directory. This is expanded with
+		/// macros to determine where the project settings will be stored. If
+		/// this is blank, then the project settings will be stored in the project
+		/// file.
+		/// </summary>
+		public string ProjectSettingsDirectory { get; set; }
+
+		#endregion
+	}
+}
---
+++ src/AuthorIntrusion.Common/Projects/ProjectMacros.cs
@@ -0,0 +1,68 @@
+﻿// Copyright 2012-2013 Moonfire Games
+// Released under the MIT license
+// http://mfgames.com/author-intrusion/license
+
+using Antlr4.StringTemplate;
+using C5;
+
+namespace AuthorIntrusion.Common.Projects
+{
+	/// <summary>
+	/// Defines the macro storage and expansion class. This handles the various
+	/// macro variables
+	/// </summary>
+	public class ProjectMacros
+	{
+		#region Properties
+
+		/// <summary>
+		/// Gets the macros and their values.
+		/// </summary>
+		public HashDictionary<string, string> Substitutions { get; private set; }
+
+		#endregion
+
+		#region Methods
+
+		/// <summary>
+		/// Expands the macros in the given string using the variables stored inside
+		/// this object. The resulting string will have the macros expanded.
+		/// </summary>
+		/// <param name="input">The input string.</param>
+		/// <returns>The output string with macros expanded.</returns>
+		public string ExpandMacros(string input)
+		{
+			// We have to repeatedly run through the macros since we have macros
+			// that expand into other macros.
+			string results = input;
+
+			while (results.IndexOf('<') >= 0)
+			{
+				// Create a template with all of the variables inside it.
+				var template = new Template(results, '<', '>');
+
+				foreach (KeyValuePair<string, string> macro in Substitutions)
+				{
+					template.Add(macro.Key, macro.Value);
+				}
+
+				// Render out the template to the results.
+				results = template.Render();
+			}
+
+			// Return the resulting string.
+			return results;
+		}
+
+		#endregion
+
+		#region Constructors
+
+		public ProjectMacros()
+		{
+			Substitutions = new HashDictionary<string, string>();
+		}
+
+		#endregion
+	}
+}
