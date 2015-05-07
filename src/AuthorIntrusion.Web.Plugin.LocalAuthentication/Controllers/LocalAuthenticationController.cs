05dc406c03f76b5ebe5158841f68f42f081cfad4
Dylan R. E. Moonfire <d.moonfire@mfgames.com>
2015-05-07 00:35:38 -0500

    Randomly trying out ideas with DI.

---
+++ src/AuthorIntrusion.Web.Plugin.LocalAuthentication/Controllers/LocalAuthenticationController.cs
@@ -0,0 +1,23 @@
+﻿// <copyright file="LocalAuthenticationController.cs" company="Moonfire Games">
+//   Copyright (c) Moonfire Games. Some Rights Reserved.
+// </copyright>
+// <license href="http://mfgames.com/mfgames-cil/license">
+//   MIT License (MIT)
+// </license>
+
+using Microsoft.AspNet.Mvc;
+
+namespace AuthorIntrusion.Web.Plugin.LocalAuthentication.Controllers
+{
+	public class LocalAuthenticationController : Controller
+	{
+		#region Public Methods and Operators
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		#endregion
+	}
+}
